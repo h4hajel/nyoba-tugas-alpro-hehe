7d7775b6718588e9edd805fffbe2c6d0a4f80f30
h4hajel <intan9el@gmail.com>
2024-10-28 23:11:35 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
-﻿/* Nama  : Intan Nurlistiyani
+/* Nama  : Intan Nurlistiyani
    NIM   : 102022400005
    Kelas : SI4808*/
 
 Console.WriteLine("Indeks Prestasi Semester");
 Console.WriteLine("========================");
-Console.Write("Input IPS anda: ");
-double IPS0005 = Convert.ToDouble(Console.ReadLine());
+double IPS0005 = 2.3;
 
+System.Console.WriteLine("IPS: " + IPS0005);
 if (IPS0005 >= 3.5 && IPS0005 <= 4)
 {
-    Console.WriteLine($"IPS: {IPS0005}, Selamat! Pertahankan IPS mu!");
+    Console.WriteLine("Selamat! Pertahankan IPS mu!");
 }
 
 else if (IPS0005 >= 3 && IPS0005 < 3.5)
 {
-    Console.WriteLine($"IPS: {IPS0005}, IPS baik, tingkatkan di semester depan!");
+    Console.WriteLine("IPS baik, tingkatkan di semester depan!");
 }
 
 else if (IPS0005 >= 2.5 && IPS0005 < 3 )
 {
-    Console.WriteLine($"IPS: {IPS0005}, IPS anda cukup baik");
+    Console.WriteLine("IPS anda cukup baik");
 }
 
 else if (IPS0005 >= 1 && IPS0005 < 2.5 )
 {
-    Console.WriteLine($"IPS: {IPS0005}, IPS anda rendah, tingkatkan di semester depan!");
+    Console.WriteLine("IPS anda rendah, tingkatkan di semester depan!");
 }
 
 else
